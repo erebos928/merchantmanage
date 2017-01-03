dafae436737cd1cba2bbb22d8ea947678eca689d
behrooz <shahin.behrooz@gmail.com>
2017-01-03 16:25:20 -0500

    Add project files.

---
+++ MerchantManage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MerchantManage.MvcApplication" Language="C#" %>
---
+++ MerchantManage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MerchantManage.Startup))]
+namespace MerchantManage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
