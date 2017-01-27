ccf063f2ce9d7252efd862d0f822f3006239c5c0
Mahboobeh <Admin@MB>
2017-01-27 10:55:13 -0500

    test2

--- MerchantManage/Controllers/AdminController.cs
+++ MerchantManage/Controllers/AdminController.cs
@@ -23,7 +23,7 @@ namespace MerchantManage.Controllers
 
             //Read data from table tbmerchant
             ViewBag.Results = merchantManagerFactory.CreateMerchantManager().GetAll();
-            return View();
+            return View("Result");
         }
         //Edit a merchant 
         public ActionResult EditMerchant(int id)
